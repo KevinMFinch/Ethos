d881a95dbd0a7576c763d0718ea04e3f012b86c9
Kevin <finchkevinm74@gmail.com>
2017-07-21 19:31:19 -0400

    Server skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
-var MongoClient = require('mongodb').MongoClient;
-
 var express = require('express');
 var router = express.Router();
 
-var mongoUrl = "mongodb://localhost:27017/jl-memes";
 
-MongoClient.connect(mongoUrl, function(err, db) {
-  /* GET home page. */
-  var collection = db.collection("documents");
-
-  router.get('/', function(req, res, next) {
-    collection.find({}).toArray(function(err, docs) {
-      var response = JSON.parse(JSON.stringify(docs));
-      res.json(response);
-    })
-  });
-
-})
-
-
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Hit Homepage');
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
+
+var MongoClient = require('mongodb').MongoClient;
+
 var express = require('express');
 var router = express.Router();
 
+var mongoUrl = "mongodb://localhost:27017/jl-memes";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hit users');
-});
+MongoClient.connect(mongoUrl, function(err, db) {
+  /* GET home page. */
+  var collection = db.collection("documents");
+
+  router.get('/', function(req, res, next) {
+    collection.find({}).toArray(function(err, docs) {
+      res.send(JSON.stringify(docs));
+    })
+  });
+
+})
+
+
 
 module.exports = router;
+
