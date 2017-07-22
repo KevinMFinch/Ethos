301db27fc394cd4e1cfc8ef73aae757ea00d864e
Kevin <finchkevinm74@gmail.com>
2017-07-21 20:26:17 -0400

    Switch to users collection

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,11 @@ var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 var router = express.Router();
 
-var mongoUrl = "mongodb://localhost:27017/jl-memes";
+var mongoUrl = "mongodb://localhost:27017/Ethos";
 
 MongoClient.connect(mongoUrl, function(err, db) {
   /* GET home page. */
-  var collection = db.collection("documents");
+  var collection = db.collection("users");
 
   router.get('/', function(req, res, next) {
     collection.find({}).toArray(function(err, docs) {
